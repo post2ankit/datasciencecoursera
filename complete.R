a2c3813d10d3bc3de97cdce257b0097e78fde63f
Ankit Gupta <agupta232@sapient.com>
2016-06-24 12:50:11 +0530

    removing unwanted files

--- complete.R
+++
@@ -1,9 +0,0 @@
-complete <- function(directory, id = 1:332){
-  filNames <- sprintf("%03d.csv", id)
-  complCases <- c()
-  for(i in filNames){
-    dat <- read.csv(file.path(directory, i), na.strings = "NA") 
-    complCases <- c(complCases, nrow(dat[complete.cases(dat),]))
-  }  
-  data.frame(id, nobs=complCases)
-}
